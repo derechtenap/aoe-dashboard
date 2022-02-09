57b96f290ba15a311b99500c31102c9c7290163c
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2022-02-09 16:59:41 +0100

    Import `steam64id` from `svelte store`

--- src/lib/Profile.svelte
+++ src/lib/Profile.svelte
@@ -5,13 +5,16 @@
 	let losses = 0;
 	let streak = 0;
 	let name = 'profile_name';
+	import { steam64id } from '../stores/settingsStore';
 </script>
 
-<h2 class="text-center text-2xl">{name}</h2>
+<h2 class="text-center text-2xl">
+	{$steam64id}
+</h2>
 <img
 	class="mx-auto mt-5 mb-10 rounded-xl"
 	src="https:via.placeholder.com/128"
-	alt={`${name}\'s Steam avatar`}
+	alt={`${$steam64id}\'s Steam avatar`}
 />
 <ul>
 	<li class="flex justify-between">Games played <var class="not-italic">{games}</var></li>
